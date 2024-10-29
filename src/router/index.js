66e58646b034a785c0eaf42d030c316d47f269b0
Ivan Vargas <42701736+mr-gantiva@users.noreply.github.com>
2024-10-29 17:26:25 -0300

    Fix lazyload on ProductosView.vue

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,8 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: ProductosView,
+    component: () =>
+      import(/* webpackChunkName: "products" */ "../views/ProductosView.vue"),
     props: () => ({
       productos: [
         {
